ca2ad0f34ef87cdea61a174454b7011af1542958
python-pro <abdumaliksharipov@gmail.com>
2019-03-18 09:12:32 -0500

    Create guessing_game(PROJECT_1).py

---
+++ guessing_game(PROJECT_1).py
@@ -0,0 +1,83 @@
+import random
+
+
+def start_game():
+	print("""
+					---------------------------------------------------
+	
+						Welcome to the number guessing game
+	
+					---------------------------------------------------
+	""")
+	
+attempts =[]
+def attempt(guess):
+	attempts.append(guess)
+	
+	
+def condition():
+	while True:
+		prompt=int(input("Pick a number between 1 and 10  "))
+		attempt(prompt)
+	
+		if prompt < solution:
+			print("It is higher")
+		
+		elif prompt > solution:
+			print("It is lower")
+		
+		else:
+			print("""
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		
+		You've got it
+		
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		""")
+			print("""
+		^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		
+		It took you {} attempts to guess the correct number
+		
+		^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^""".format(len(attempts)))
+			break
+
+			
+def play_again():
+	while True:
+		play_again=input("""
+
+
+Would you like to play again? [y]es/[no]   """)
+		play_again=play_again.lower()
+		if play_again == 'y':
+			start_game()
+			condition()
+		elif play_again == 'n':
+			print("""
+		==========================================
+		
+				THE GAME IS OVER THEN
+				
+		==========================================
+		""")
+			break
+		else:
+			print("                Excuse me?            ")
+			print("				   Please say yes or no, pressing 'y' or 'n' accordingly          ")
+
+
+	
+  
+
+start_game()
+
+
+solution=random.randint(1,10)
+print(solution)
+
+
+condition()
+
+
+play_again()
